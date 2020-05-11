e66b031cc77c1fe8cc31d40891dedfa9a2b8285d
Liufan-YIN <65107876+Liufan-YIN@users.noreply.github.com>
2020-05-11 14:32:28 +0800

    Update ChIPseq_paired_nocut.sh

--- shell_scirpts/ChIPseq_paired_nocut.sh
+++ shell_scirpts/ChIPseq_paired_nocut.sh
@@ -20,7 +20,7 @@ samtools rmdup ${id%_R1*}_q20_s.bam   ${id%_R1*}_q20_s_rm.bam
 echo "rmdup done"
 samtools index ${id%_R1*}_q20_s_rm.bam
 echo "index done"
-samtools view -F 4 -c {id%_R1*}_q20_s.bam
+samtools view -F 4 -c ${id%_R1*}_q20_s.bam
 samtools view -F 4 -c ${id%_R1*}_q20_s_rm.bam
 echo "reads done"
 bamCoverage -b ${id%_R1*}_q20_s_rm.bam -o ${id%_R1*}_q20_s_rm_RPKM.bigwig --binSize 10 --normalizeUsing RPKM
